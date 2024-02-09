87f671ab78daf1fa3274b8eb4579be32a758ee08
mishaqq <dron7432@gmail.com>
2024-02-09 17:57:26 +0100

    Sets Screen UI done

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -1,1 +1,14 @@
 final List<String> SETS = ["basic", "expert", "ukr", "tworoots"];
+final Map<String, String> title = {
+  "basic": "Базові слова",
+  "expert": "Експерт",
+  "ukr": "Старі українські слова",
+  "tworoots": "Складні слова"
+};
+
+final Map<String, String> example = {
+  "basic": "зірка, корабель, парасолька...",
+  "expert": "багатогранний, азимут, дискусія...",
+  "ukr": "кайдаш, ушляхетнити...",
+  "tworoots": "фотосинтез, радіоприймач, зорепад..."
+};
--- lib/screens/setSelection_page.dart
+++ lib/screens/setSelection_page.dart
@@ -26,59 +26,217 @@ class _GuessingPageState extends ConsumerState<ChoosingPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: Column(
-          children: [
-            const Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Text(
-                'Select word sets',
+      body: Container(
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color.fromARGB(255, 112, 150, 236),
+              Color.fromARGB(255, 52, 104, 192)
+            ],
+          ),
+        ),
+        child: Padding(
+          padding: EdgeInsets.only(
+            top: MediaQuery.of(context).size.height * 0.09,
+            left: MediaQuery.of(context).size.width * 0.075,
+            right: MediaQuery.of(context).size.width * 0.075,
+          ),
+          child: Column(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 248, 237, 255),
+                  border: Border.all(
+                      width: MediaQuery.of(context).size.height * 0.0024),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(MediaQuery.of(context).size.height * 0.018),
+                  ),
+                ),
+                width: MediaQuery.of(context).size.width * 0.85,
+                height: MediaQuery.of(context).size.height * 0.75,
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(
+                          top: MediaQuery.of(context).size.height * 0.015),
+                      child: Text(
+                        'Слова',
+                      ),
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: EdgeInsets.only(
+                          left: MediaQuery.of(context).size.height * 0.015,
+                          right: MediaQuery.of(context).size.height * 0.015,
+                        ),
+                        child: MultiSelectCheckList(
+                          itemPadding: EdgeInsets.all(
+                              MediaQuery.of(context).size.height * 0.01),
+                          maxSelectableCount: 5,
+                          textStyles: MultiSelectTextStyles(
+                            selectedTextStyle: TextStyle(
+                                fontWeight: FontWeight.w700,
+                                color: Colors.black,
+                                fontFamily: "Anonymous",
+                                fontSize:
+                                    MediaQuery.of(context).size.height * 0.020),
+                          ),
+                          itemsDecoration: MultiSelectDecorations(
+                            selectedDecoration: BoxDecoration(
+                              color: Color.fromARGB(255, 255, 221, 149),
+                              border: Border.all(
+                                  width: MediaQuery.of(context).size.height *
+                                      0.0015),
+                            ),
+                          ),
+                          listViewSettings: ListViewSettings(
+                            physics: const BouncingScrollPhysics(),
+                          ),
+                          controller: _controller,
+                          items: List.generate(
+                            SETS.length,
+                            (index) => CheckListCard(
+                              contentPadding: EdgeInsets.all(0),
+                              decorations: MultiSelectItemDecorations(
+                                decoration: BoxDecoration(
+                                  border: Border.all(
+                                      width:
+                                          MediaQuery.of(context).size.height *
+                                              0.0015),
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(
+                                        MediaQuery.of(context).size.height *
+                                            0.018),
+                                  ),
+                                ),
+                              ),
+                              textStyles: MultiSelectItemTextStyles(
+                                textStyle: TextStyle(
+                                  fontWeight: FontWeight.w700,
+                                  color: Colors.black,
+                                  fontFamily: "Anonymous",
+                                  fontSize: MediaQuery.of(context).size.height *
+                                      0.020,
+                                ),
+                              ),
+                              value: SETS[index],
+                              title: Text(title[SETS[index]]!),
+                              subtitle: Padding(
+                                padding: EdgeInsets.only(
+                                    top: MediaQuery.of(context).size.height *
+                                        0.0020),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    Padding(
+                                      padding: EdgeInsets.only(
+                                          top: MediaQuery.of(context)
+                                                  .size
+                                                  .height *
+                                              0.0015),
+                                      child: Text(
+                                        "${setsTable[SETS[index]]!.length} Слів",
+                                        style: TextStyle(
+                                            fontSize: MediaQuery.of(context)
+                                                    .size
+                                                    .height *
+                                                0.015),
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding: EdgeInsets.only(
+                                          top: MediaQuery.of(context)
+                                                  .size
+                                                  .height *
+                                              0.0015),
+                                      child: Wrap(
+                                        children: [
+                                          Text(
+                                            "${example[SETS[index]]}",
+                                            style: TextStyle(
+                                                fontStyle: FontStyle.italic,
+                                                fontSize: MediaQuery.of(context)
+                                                        .size
+                                                        .height *
+                                                    0.015),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              selectedColor: Colors.white,
+                              checkColor: Colors.black,
+                              leadingCheckBox: false,
+                              checkBoxBorderSide:
+                                  const BorderSide(color: Colors.black),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(
+                                    MediaQuery.of(context).size.height * 0.005),
+                              ),
+                            ),
+                          ),
+                          onChange: (allSelectedItems, selectedItem) {},
+                          onMaximumSelected:
+                              (allSelectedItems, selectedItem) {},
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            Expanded(
-              child: MultiSelectCheckList(
-                maxSelectableCount: 5,
-                textStyles: const MultiSelectTextStyles(
-                    selectedTextStyle: TextStyle(
-                        color: Colors.white, fontWeight: FontWeight.bold)),
-                itemsDecoration: MultiSelectDecorations(
-                    selectedDecoration:
-                        BoxDecoration(color: Colors.indigo.withOpacity(0.8))),
-                listViewSettings: ListViewSettings(
-                    separatorBuilder: (context, index) => const Divider(
-                          height: 0,
-                        )),
-                controller: _controller,
-                items: List.generate(
-                    SETS.length,
-                    (index) => CheckListCard(
-                        value: SETS[index],
-                        title: Text(SETS[index]),
-                        subtitle:
-                            Text("${setsTable[SETS[index]]!.length} Words"),
-                        selectedColor: Colors.white,
-                        checkColor: Colors.indigo,
-                        leadingCheckBox: false,
-                        checkBoxBorderSide:
-                            const BorderSide(color: Colors.blue),
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(5)))),
-                onChange: (allSelectedItems, selectedItem) {},
-                onMaximumSelected: (allSelectedItems, selectedItem) {},
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                ref
-                    .read(gameProvider.notifier)
-                    .makeGameWordSet(_controller.getSelectedItems());
+              Padding(
+                padding: EdgeInsets.only(
+                  top: MediaQuery.of(context).size.height * 0.01,
+                ),
+                child: Row(
+                  children: [
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.18,
+                      height: MediaQuery.of(context).size.height * 0.06,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          foregroundColor: Colors.black,
+                          backgroundColor: Color.fromARGB(255, 255, 221, 149),
+                        ),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        child: Icon(
+                          Icons.arrow_back_ios_new_rounded,
+                          size: MediaQuery.of(context).size.height * 0.03,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(
+                        left: MediaQuery.of(context).size.width * 0.02,
+                      ),
+                      child: SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.65,
+                        height: MediaQuery.of(context).size.height * 0.06,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            ref.read(gameProvider.notifier).makeGameWordSet(
+                                _controller.getSelectedItems());
 
-                Navigator.pushNamed(context, "/settings");
-              },
-              child: Text("Confirm"),
-            ),
-          ],
+                            Navigator.pushNamed(context, "/settings");
+                          },
+                          child: Text("Продовжити",
+                              style: Theme.of(context).textTheme.bodyMedium),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
